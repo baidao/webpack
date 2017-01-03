5a052a8c2aff7491ada0b6ceee565ed1c0176b55
stone <stone.grandia@gmail.com>
2017-01-03 16:39:23 +0800

    修改eslint规则

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -41,6 +41,9 @@ module.exports = {
     }],
     {{/if_eq}}
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    "space-before-function-paren": [0, "always"],
+    "semi": [2, "always"],
+    "indent": [2, 4]
   }
 }
