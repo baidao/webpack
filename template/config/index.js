a856f9733da482ce30138a04cd3fa9e06f3c804b
郭令恩 <lingen.guo@baidao.com>
2017-02-09 16:06:55 +0800

    fix bug

--- template/config/index.js
+++ template/config/index.js
@@ -28,7 +28,7 @@ module.exports = {
   dev: {
     env: require('./dev.env'),
     port: 8080,
-    assetsSubDirectory: 'static',
+    assetsSubDirectory: '/static',
     assetsPublicPath: './',
     proxyTable: {},
     // CSS Sourcemaps off by default because relative paths are "buggy"
